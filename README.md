e2c3c097b807f182f4c164970c969cbb50e134e0
André Luiz Albano <andre_barros@id.uff.br>
2024-04-15 15:34:24 -0300

    v0.7.1.00001
    Updated translation
    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Petrofizika
  A simple petrophysics calculator
+
+# UNDER CONSTRUCTION
+
+To-do list:
+* TODO: Implement Porosity > Resistivity tab.
+* TODO: Implement Porosity > Shale Correction tab.
+* TODO: Implement Permeability tab.
+* TODO: Implement Resistivity tab.
+* TODO: Implement Water Saturation class.
+* TODO: Implement Water Saturation tab.
+* TODO: Implement Reserves tab.
--- src/interface/mainWindow.ts
+++ src/interface/mainWindow.ts
@@ -32,6 +32,8 @@
         <location filename="mainWindow.ui" line="300"/>
         <location filename="mainWindow.ui" line="513"/>
         <location filename="mainWindow.ui" line="573"/>
+        <location filename="mainWindow.ui" line="859"/>
+        <location filename="mainWindow.ui" line="1096"/>
         <source>Result (dec)</source>
         <translation>Resultado (dec)</translation>
     </message>
@@ -40,6 +42,8 @@
         <location filename="mainWindow.ui" line="284"/>
         <location filename="mainWindow.ui" line="503"/>
         <location filename="mainWindow.ui" line="563"/>
+        <location filename="mainWindow.ui" line="846"/>
+        <location filename="mainWindow.ui" line="1114"/>
         <source>Result (%)</source>
         <translation>Resultado(%)</translation>
     </message>
@@ -74,6 +78,8 @@
         <location filename="mainWindow.ui" line="314"/>
         <location filename="mainWindow.ui" line="493"/>
         <location filename="mainWindow.ui" line="580"/>
+        <location filename="mainWindow.ui" line="879"/>
+        <location filename="mainWindow.ui" line="1005"/>
         <source>Calculate</source>
         <translation>Calcular</translation>
     </message>
@@ -165,6 +171,7 @@
     </message>
     <message>
         <location filename="mainWindow.ui" line="651"/>
+        <location filename="mainWindow.ui" line="1162"/>
         <source>Add to the Table</source>
         <translation>Adicionar à Tabela</translation>
     </message>
@@ -200,155 +207,255 @@
         <translation>Volume de Argila</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="755"/>
+        <location filename="mainWindow.ui" line="756"/>
+        <source>Shale Volume using different methods</source>
+        <translation>Volume de Argila usando diferentes métodos</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="768"/>
+        <source>Using Spoteaneous Potential</source>
+        <translation>Usando o Potencial Espontâneo (SP)</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="776"/>
+        <location filename="mainWindow.ui" line="990"/>
+        <source>Choose a method:</source>
+        <translation>Escolha um método:</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="787"/>
+        <source>Standard</source>
+        <translation>Padrão</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="792"/>
+        <source>Alternative</source>
+        <translation>Alternativo</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="800"/>
+        <source>PSP</source>
+        <translation>PSP</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="810"/>
+        <source>SSP</source>
+        <translation>SSP</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="823"/>
+        <location filename="mainWindow.ui" line="833"/>
+        <location filename="mainWindow.ui" line="900"/>
+        <source>mV</source>
+        <translation>mV</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="869"/>
+        <source>SPshale</source>
+        <translation>SPshale</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="931"/>
+        <source>Output of IGR (Decimal)</source>
+        <translation>IGR (Decimal)</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="943"/>
+        <source>GRlog</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="962"/>
+        <source>Larionov</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="967"/>
+        <source>Larionov - Old Rocks</source>
+        <translation>Larionov - Rochas Antigas</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="972"/>
+        <source>Steiber</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="977"/>
+        <source>Clavier</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="1027"/>
+        <location filename="mainWindow.ui" line="1042"/>
+        <location filename="mainWindow.ui" line="1129"/>
+        <source>API</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="1054"/>
+        <source>GRmin</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="1075"/>
+        <source>GRmax</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="1141"/>
+        <source>IGR:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.ui" line="1229"/>
         <source>Reserves</source>
         <translation>Reservas</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="760"/>
+        <location filename="mainWindow.ui" line="1234"/>
         <source>Conversions</source>
         <translation>Conversões</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="783"/>
+        <location filename="mainWindow.ui" line="1257"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:18pt; font-weight:600;&quot;&gt;Distance&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:18pt; font-weight:600;&quot;&gt;Distância&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="807"/>
-        <location filename="mainWindow.ui" line="844"/>
+        <location filename="mainWindow.ui" line="1281"/>
+        <location filename="mainWindow.ui" line="1318"/>
         <source>Feet</source>
         <translation>Pés</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="812"/>
-        <location filename="mainWindow.ui" line="849"/>
+        <location filename="mainWindow.ui" line="1286"/>
+        <location filename="mainWindow.ui" line="1323"/>
         <source>Meters</source>
         <translation>Metros</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="817"/>
-        <location filename="mainWindow.ui" line="854"/>
+        <location filename="mainWindow.ui" line="1291"/>
+        <location filename="mainWindow.ui" line="1328"/>
         <source>Miles</source>
         <translation>Milhas</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="822"/>
-        <location filename="mainWindow.ui" line="859"/>
+        <location filename="mainWindow.ui" line="1296"/>
+        <location filename="mainWindow.ui" line="1333"/>
         <source>Kilometers</source>
         <translation>Quilômetros</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="830"/>
-        <location filename="mainWindow.ui" line="908"/>
-        <location filename="mainWindow.ui" line="1006"/>
-        <location filename="mainWindow.ui" line="1073"/>
+        <location filename="mainWindow.ui" line="1304"/>
+        <location filename="mainWindow.ui" line="1382"/>
+        <location filename="mainWindow.ui" line="1480"/>
+        <location filename="mainWindow.ui" line="1547"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;TO&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;PARA&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="871"/>
+        <location filename="mainWindow.ui" line="1345"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:18pt; font-weight:600;&quot;&gt;Area&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:18pt; font-weight:600;&quot;&gt;Área&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="885"/>
-        <location filename="mainWindow.ui" line="932"/>
+        <location filename="mainWindow.ui" line="1359"/>
+        <location filename="mainWindow.ui" line="1406"/>
         <source>Acres</source>
         <translation>Acres</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="890"/>
-        <location filename="mainWindow.ui" line="937"/>
+        <location filename="mainWindow.ui" line="1364"/>
+        <location filename="mainWindow.ui" line="1411"/>
         <source>Square feet</source>
         <translation>Pés quadrados</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="895"/>
-        <location filename="mainWindow.ui" line="942"/>
+        <location filename="mainWindow.ui" line="1369"/>
+        <location filename="mainWindow.ui" line="1416"/>
         <source>Square meters</source>
         <translation>Metros quadrados</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="900"/>
-        <location filename="mainWindow.ui" line="947"/>
+        <location filename="mainWindow.ui" line="1374"/>
+        <location filename="mainWindow.ui" line="1421"/>
         <source>Square kilometers</source>
         <translation>Quilômetros quadrados</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="966"/>
-        <location filename="mainWindow.ui" line="1036"/>
+        <location filename="mainWindow.ui" line="1440"/>
+        <location filename="mainWindow.ui" line="1510"/>
         <source>Acre-feet</source>
         <translation>Acre-pés</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="971"/>
-        <location filename="mainWindow.ui" line="1041"/>
+        <location filename="mainWindow.ui" line="1445"/>
+        <location filename="mainWindow.ui" line="1515"/>
         <source>Barrels</source>
         <translation>Barris</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="976"/>
-        <location filename="mainWindow.ui" line="1046"/>
+        <location filename="mainWindow.ui" line="1450"/>
+        <location filename="mainWindow.ui" line="1520"/>
         <source>Cubic feet</source>
         <translation>Pés cúbicos</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="981"/>
-        <location filename="mainWindow.ui" line="1051"/>
+        <location filename="mainWindow.ui" line="1455"/>
+        <location filename="mainWindow.ui" line="1525"/>
         <source>Cubic meters</source>
         <translation>Metros cúbicos</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="986"/>
-        <location filename="mainWindow.ui" line="1056"/>
+        <location filename="mainWindow.ui" line="1460"/>
+        <location filename="mainWindow.ui" line="1530"/>
         <source>Gallons</source>
         <translation>Galões</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="991"/>
-        <location filename="mainWindow.ui" line="1061"/>
+        <location filename="mainWindow.ui" line="1465"/>
+        <location filename="mainWindow.ui" line="1535"/>
         <source>Metric tons</source>
         <translation>Toneladas métricas</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="999"/>
+        <location filename="mainWindow.ui" line="1473"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:18pt; font-weight:600;&quot;&gt;Volume&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:18pt; font-weight:600;&quot;&gt;Volume&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="1090"/>
+        <location filename="mainWindow.ui" line="1564"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:18pt; font-weight:600;&quot;&gt;Pressure&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:18pt; font-weight:600;&quot;&gt;Pressão&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="1098"/>
-        <location filename="mainWindow.ui" line="1127"/>
+        <location filename="mainWindow.ui" line="1572"/>
+        <location filename="mainWindow.ui" line="1601"/>
         <source>PSIA</source>
         <translatorcomment>Libras por polegada quadrada</translatorcomment>
         <translation>PSIA</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="1103"/>
-        <location filename="mainWindow.ui" line="1132"/>
+        <location filename="mainWindow.ui" line="1577"/>
+        <location filename="mainWindow.ui" line="1606"/>
         <source>kPa</source>
         <translation>kPa</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="1108"/>
-        <location filename="mainWindow.ui" line="1137"/>
+        <location filename="mainWindow.ui" line="1582"/>
+        <location filename="mainWindow.ui" line="1611"/>
         <source>Bar</source>
         <translation>Bar</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="1113"/>
-        <location filename="mainWindow.ui" line="1142"/>
+        <location filename="mainWindow.ui" line="1587"/>
+        <location filename="mainWindow.ui" line="1616"/>
         <source>BTU</source>
         <translation>BTU</translation>
     </message>
     <message>
-        <location filename="mainWindow.ui" line="1118"/>
-        <location filename="mainWindow.ui" line="1147"/>
+        <location filename="mainWindow.ui" line="1592"/>
+        <location filename="mainWindow.ui" line="1621"/>
         <source>Joules</source>
         <translation>Joules</translation>
     </message>
